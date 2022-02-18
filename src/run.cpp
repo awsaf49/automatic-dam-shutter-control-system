a44721d06d388272c19a0b83d104fcd31fdf55c2
Awsaf <awsaf49@gmail.com>
2022-02-18 10:28:43 +0600

    refactor
    1. remove `#define`
    2. use `byte` for pin
    3. add comment

--- src/run.cpp
+++ src/run.cpp
@@ -1,18 +1,21 @@
-#define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
-#define heightDam 200 //height of the dam in cm
-#define motorPin1 6 //motor pin 1
-#define motorPin2 7 //motor pin 2
+// define constants
+const byte echoPin = 2; // attach pin D2 Arduino to pin Echo of HC-SR04
+const byte trigPin = 3; //attach pin D3 Arduino to pin Trig of HC-SR04
+const byte motorPin1 = 6; //motor pin 1
+const byte motorPin2 = 7; //motor pin 2
+  
+const unsigned long heightDam = 200; //height of the dam in cm
+const unsigned long openHeight = 100; //height to open the gate
+const unsigned long closeHeight = 50; //height to close the gate
 
-const unsigned long openHeight = 100;
-const unsigned long closeHeight = 50;
+const unsigned long openTime = 500;  //time to open gate in ms
+const unsigned long closeTime = 300; //time to close gate in ms
 
-const unsigned long openTime = 1000;  //time to open gate in ms
-const unsigned long closeTime = 900; //time to close gate in ms
+const double windSpeed = 0.034; // speed of wind cm/us
 
-// defines variables
-long duration; // variable for the duration of sound wave travel
-int distance; // variable for the distance measurement
+//defines variables
+long duration; //variable for the duration of sound wave travel
+int distance; //variable for the distance measurement
 
 float openTik = -1;
 float openTok = -1;
@@ -20,14 +23,14 @@ float openTok = -1;
 float closeTik = -1;
 float closeTok = -1;
 
-bool isOpen = false;
+bool isOpen = false; //gate is open or not
 
 void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
-  Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
+  Serial.begin(9600); // Serial Communication is starting with 9600 of baudrate speed
   Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
   Serial.println("with Arduino UNO R3");
 }
@@ -42,7 +45,7 @@ void loop() {
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
-  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  distance = duration * windSpeed / 2; // Speed of sound wave divided by 2 (go and back)
   // Displays the distance on the Serial Monitor
   Serial.print("Distance: ");
   Serial.print(distance);
